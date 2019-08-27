94de67cf82a41e70ccc5a8b303ba6b9d2d941e44
RuslanCC <mail@ruslan.cc>
2019-08-27 09:37:26 +0300

    6

--- assets/js/bs-animation.js
+++ assets/js/bs-animation.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 	$('[data-bs-hover-animate]')
-		.touchstart( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.touchleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
-});
-
-/*
-
-$(document).ready(function(){
-	$('[data-bs-hover-animate]')
 		.click( function(){ 
 			var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate'));
 			setTimeout(function(elem) { elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')); }, 1000);
 		 });
 
 });
+/*
+
+
+
+$(document).ready(function(){
+	$('[data-bs-hover-animate]')
+		.touchstart( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
+		.touchleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
+});
+
 
 */
